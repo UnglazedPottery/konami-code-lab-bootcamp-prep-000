fcdb440eb9315cde9038e12fa610ab243e3b2703
Jade Huynh <35jadeh@gmail.com>
2019-07-10 13:56:57 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,25 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
+function init(){
+  let index = 0;
+  document.body.addEventListener('keydown', onKeyDownHandler);
+  
+  function onKeyDownHandler(e){
+    const key = e.key;
+    console.log(index);
+    
+    if(key === codes[index])
+    {
+      index++;
+      
+      if(index === codes.length)
+      {
+        alert("I sense konami.");
+        index = 0;
+      }
+    }else{index = 0;}
+  }
 }
+
+//init();
